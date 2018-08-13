566821d84e28324bace1bdb995df0df052bda06e
silver(Alter) <mj_ol@outlook.com>
2018-08-13 04:25:02 -0400

    ADD:Add functions to complete

--- demo.py
+++ demo.py
@@ -7,16 +7,29 @@ class QQInstance():
         except:
             print "Login Failed"
         else:
+            self.bot = bot
             self.get_buddy_list()
             self.get_group_list()
             
 
     def get_buddy_list(self):
-        self.buddy_list = bot.List('buddy')
+        self.buddy_list = self.bot.List('buddy')
     
     def get_group_list(self):
-        self.group_list = bot.List('group')
+        self.group_list = self.bot.List('group')
     
+    def send_message(contact,message):
+        try:
+            self.bot.SendTo()
+        except:
+            raise "Send Failed,check your network or restart this application"
+        else:
+            pass
+            #show sended message on windows
+
+    def 
+
+
     #Generate recent chat list and friends or group chats list
     def generate_lists(self):
         return 0    
@@ -31,3 +44,4 @@ class QQInstance():
     #Trigger on new message comes
     def update_state(self):
         #update the information of each title
+        #update the chatting windows information
