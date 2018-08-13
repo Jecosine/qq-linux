10dc052889efddcfd78b62eb2753ca4468d9b7d5
silver(Alter) <mj_ol@outlook.com>
2018-08-13 03:23:28 -0400

    ADD:Add functions to complete

--- demo.py
+++ demo.py
@@ -1,2 +1,33 @@
 from qqbot import _bot as bot
 
+class QQInstance():
+    def __init__(self):
+        try:
+            bot.Login()
+        except:
+            print "Login Failed"
+        else:
+            self.get_buddy_list()
+            self.get_group_list()
+            
+
+    def get_buddy_list(self):
+        self.buddy_list = bot.List('buddy')
+    
+    def get_group_list(self):
+        self.group_list = bot.List('group')
+    
+    #Generate recent chat list and friends or group chats list
+    def generate_lists(self):
+        return 0    
+    
+    #Generate chat window after double click
+    def popup_chat_window(self):
+        #popup new window
+
+    def initialize_main_window(self):
+        #set layouts and fill informations
+
+    #Trigger on new message comes
+    def update_state(self):
+        #update the information of each title
